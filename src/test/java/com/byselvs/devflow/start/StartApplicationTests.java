237d9e62401ea4f3bc133eca08fffbd48bcd6928
Selvino Junior <selvs@byselvs.com>
2024-09-16 00:00:42 -0300

    subindo estrutura do projeto feito pelo sprint initializer

---
+++ src/test/java/com/byselvs/devflow/start/StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byselvs.devflow.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
